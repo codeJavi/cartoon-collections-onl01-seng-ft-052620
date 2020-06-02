f80f852e79f0ac0f0fe44c0d70d4e64ba4e2845f
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 22:33:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,17 @@
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
+
 def roll_call_dwarves(array)
   i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
     i += 1 
   end 
+end
+
 roll_call_dwarves(dwarves)
+
 
 
 def summon_captain_planet(array)
@@ -16,6 +23,7 @@ def summon_captain_planet(array)
   end 
   nu_array
 end
+
 summon_captain_planet(planeteer_calls)
 
 
@@ -27,7 +35,13 @@ def long_planeteer_calls(array)
     return false
   i = i + 1
   end 
+end
+
 long_planeteer_calls(call_screams)
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
 
 
 def find_the_cheese(array)
@@ -35,13 +49,21 @@ def find_the_cheese(array)
     array.find do |type|
     cheese_types.include?(type)
   end 
+end
+
 find_the_cheese(snacks)
 
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese2(array, array2)
   cheese_types = array2
     array.find do |type|
     cheese_types.include?(type)
   end 
+end
+
 find_the_cheese2(snacks, cheese_types)
 
