98d98b2a141c12b6d5c02e4fb15afb5e0679544d
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 22:17:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,4 @@
-def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
-end
 
-roll_call_dwarves(dwarves)
 
 def summon_captain_planet(array)
   nu_array = []
