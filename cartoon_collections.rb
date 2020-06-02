330feac7df29264a9d324a083c7dedc506706fb1
Javi Leyva <jvrleyva@gmail.com>
2020-06-02 22:31:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ def roll_call_dwarves(array)
   end 
 roll_call_dwarves(dwarves)
 
+
 def summon_captain_planet(array)
   nu_array = []
   i = 0 
@@ -15,8 +16,8 @@ def summon_captain_planet(array)
   end 
   nu_array
 end
-
 summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls(array) 
   i = 0 
@@ -26,25 +27,21 @@ def long_planeteer_calls(array)
     return false
   i = i + 1
   end 
-
-
 long_planeteer_calls(call_screams)
+
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
   end 
-
-
 find_the_cheese(snacks)
+
 
 def find_the_cheese2(array, array2)
   cheese_types = array2
     array.find do |type|
     cheese_types.include?(type)
   end 
-
-
 find_the_cheese2(snacks, cheese_types)
-end 
+
